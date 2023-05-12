96efc542fb873860b4840ad9959dd7821866a4f7
DoDuke1810 <hung.20071810.903@gmail.com>
2023-05-12 20:31:59 +0700

    col

--- st.py
+++ st.py
@@ -1,19 +1,13 @@
 import streamlit as st
-st.title('Bé tập làm toán')
-a = st.number_input('a')
-radio = st.radio(label='Phép toán', options=('+', '-', 'x', ':'), horizontal=True)
-b = st.number_input('b')
-text = st.text_input('Nhập kết quả')
-if st.button('Kiểm tra'):
-    result = a + b
-    if radio == '-':
-        result = a - b
-    elif radio == 'x':
-        result = a * b
-    elif radio == ':':
-        result = a / b
+col1, col2, col3 = st.columns(3)
 
-    if result == float(text):
-        st.success('Chính xác')
-    else:
-        st.error(f'Sai rồi, đáp số đúng là {result}')
+with col1:
+    st.header("A cat")
+
+with col2:
+    st.header("A dog")
+
+with col3:
+    st.header("An owl")
+
+st.text('This text is outside of column')
