14e85f66efcc7b07c684d13451cfdaee69ec11fb
DoDuke1810 <110729811+DoDuke1810@users.noreply.github.com>
2023-05-12 21:08:51 +0700

    Update st.py

--- st.py
+++ st.py
@@ -2,7 +2,9 @@ import streamlit as st
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    gender = st.checkbox('Giới tính',('Nam','Nữ'))
+    gender = st.write('Giới tính')
+    male = st.checkbox('Nam')
+    female = st.checkbox('Nữ')
 
 with col2: 
     radio = st.radio('Khối lớp', ('Tất cả', 'Lớp 10', 'Lớp 11', "Lớp 12"), horizontal=True)
