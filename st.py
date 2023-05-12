d32c5dce5e9efe52940c783ec38b7bee91ce57ad
DoDuke1810 <110729811+DoDuke1810@users.noreply.github.com>
2023-05-12 21:03:44 +0700

    new

--- st.py
+++ st.py
@@ -1,7 +1,17 @@
 import streamlit as st
-check = st.checkbox('Boy')
-radio = st.radio('Radio', ('Option 1', 'Option 2', 'Option 3'), horizontal=True)
-if st.button('OK'):
-  st.write('Check box:', check)
-  st.write('Radio:', radio, type(radio))
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    st.subheader('Giới tính')
+    male = st.checkbox('Nam')
+    female = st.checkbox('Nữ')
+
+with col2: 
+    class = st.radio('Khối lớp', ('Tất cả', 'Lớp 10', 'Lớp 11', "Lớp 12"), horizontal=True)
+
+with col3: 
+    AI_class = st.selectbox('Phòng', ('Tất cả','A114', 'A115'))
+
+with col4: 
+    times = st.multiselect('Buổi', ['Sáng','Chiều'])
   
