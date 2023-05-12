a3e76b0bc6aca3303709a978b0863e2ce4521e52
DoDuke1810 <hung.20071810.903@gmail.com>
2023-05-12 18:56:06 +0700

    try-2

--- st.py
+++ st.py
@@ -9,3 +9,9 @@ if st.button('OK'):
   st.write('text content:', text)
   st.write('number content:', number)
   st.write('text area content:', textarea)
+
+check = st.checkbox('Boy')
+radio = st.radio('Radio', ('Option 1', 'Option 2', 'Option 3'), horizontal=True)
+if st.button('OK'):
+  st.write('Check box:', check)
+  st.write('Radio:', radio, type(radio))
